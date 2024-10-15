ad90f014a54de749af59776a319a8d06a99490e0
stardust <88991140+stardustgd@users.noreply.github.com>
2024-10-15 15:02:06 -0700

    start nextjs project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-b from-[#F7BE7A] to-[#DA8359] h-screen">
+      <div className="flex justify-center items-center h-16">
+        <h1 className="text-md">Smart Pet Feeder</h1>
+      </div>
+      <div className="px-12 py-5 w-screen rounded-t-2xl bg-[#F2F2F2] text-black">
+        <h1>Smart Pet Feeder</h1>
+      </div>
+    </div>
+  )
+}

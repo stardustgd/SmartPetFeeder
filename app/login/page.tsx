f3eaa59bbc1e89f055636e4d4b113c4e5eb3e5a9
stardust <88991140+stardustgd@users.noreply.github.com>
2024-10-28 21:30:47 -0700

    add shadcn input/label and revamp login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,8 @@
 import NavBar from '@/components/ui/NavBar'
 import Link from 'next/link'
+import { FaGithub } from 'react-icons/fa'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 
 export default function LoginPage() {
@@ -8,16 +11,15 @@ export default function LoginPage() {
       <NavBar title="Login" showArrow={true} />
       <div className="flex flex-col items-center gap-3 px-5 py-5 w-screen h-fit rounded-2xl bg-[#F2F2F2] text-black">
         <h1 className="text-4xl">Sign In</h1>
-        <form className="space-y-3">
-          <input
-            placeholder="Email or Username"
-            className="p-2 rounded-md bg-gray-50 border border-gray-300 text-gray-900"
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="p-2 rounded-md bg-gray-50 border border-gray-300 text-gray-900"
-          />
+        <form className="w-full space-y-3 pb-5">
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label>Username</Label>
+            <Input type="email" />
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label>Password</Label>
+            <Input type="password" />
+          </div>
         </form>
         <Button className="bg-[#F7BE7A] hover:bg-[#DA8359] w-full">
           Login
@@ -27,7 +29,10 @@ export default function LoginPage() {
           <span className="flex-shrink mx-4 text-gray-400">or</span>
           <div className="flex-grow border-t border-gray-400"></div>
         </div>
-        <Button>Continue with GitHub</Button>
+        <Button className="flex gap-2">
+          <FaGithub />
+          Continue with GitHub
+        </Button>
         <Link href="/register/">
           <h1 className="text-center text-bold">
             Need an account? <span className="text-blue-600">Register</span>

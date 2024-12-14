4353483bd4e27f699186610d971817d5d17861ac
stardust <88991140+stardustgd@users.noreply.github.com>
2024-12-13 19:02:01 -0800

    add viewport to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
 
@@ -18,6 +18,12 @@ export const metadata: Metadata = {
   description: 'Smart Pet Feeder',
 }
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -25,10 +31,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1"
-      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#F7BE7A]`}
       >

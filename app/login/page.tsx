c01735f89141b055f807ef6b2adef1b1cdb98309
stardust <88991140+stardustgd@users.noreply.github.com>
2024-12-13 18:41:44 -0800

    implement form validation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,35 +1,84 @@
-import NavBar from '@/components/ui/NavBar'
+'use client'
+
 import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { FaGithub } from 'react-icons/fa'
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
+
+import NavBar from '@/components/ui/NavBar'
 import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { LoginSchema } from '@/schema'
 
 export default function LoginPage() {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof LoginSchema>) {
+    console.log(values)
+  }
+
   return (
     <>
       <NavBar title="Login" showArrow={true} />
       <div className="flex flex-col items-center gap-3 px-5 py-5 w-screen h-fit rounded-2xl bg-[#F2F2F2] text-black">
         <h1 className="text-4xl">Sign In</h1>
-        <form className="w-full space-y-3 pb-5">
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label>Username</Label>
-            <Input type="email" />
-          </div>
-          <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label>Password</Label>
-            <Input type="password" />
-          </div>
-        </form>
-        <Button className="bg-[#F7BE7A] hover:bg-[#DA8359] w-full">
-          Login
-        </Button>
-        <div className="w-full relative flex py-5 items-center">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-3 pb-5"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </form>
+        </Form>
+        <div className="w-full relative flex pb-5 items-center">
           <div className="flex-grow border-t border-gray-400"></div>
           <span className="flex-shrink mx-4 text-gray-400">or</span>
           <div className="flex-grow border-t border-gray-400"></div>
         </div>
-        <Button className="flex gap-2">
+        <Button className="flex gap-2 bg-black">
           <FaGithub />
           Continue with GitHub
         </Button>

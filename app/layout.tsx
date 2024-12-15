700243f6a0508e8a3e6a31a0caa442daae573f53
stardustgd <88991140+stardustgd@users.noreply.github.com>
2024-12-14 18:14:23 -0800

    Fix zoom issue with inputs (#5)
    * add maximum-scale=1 to fix zoom issue
    
    * add viewport to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
 
@@ -16,6 +16,12 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: 'Smart Pet Feeder',
   description: 'Smart Pet Feeder',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
 }
 
 export default function RootLayout({

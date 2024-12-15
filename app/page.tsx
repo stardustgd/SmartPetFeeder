9a062a37cc8623678050669daa02a79800a28f7b
stardust <88991140+stardustgd@users.noreply.github.com>
2024-12-14 19:03:31 -0800

    revamp home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,34 +3,46 @@ import Image from 'next/image'
 import NavBar from '@/components/ui/NavBar'
 import { Button } from '@/components/ui/button'
 
+type MenuButtonProps = {
+  href: string
+  title: string
+}
+function MenuButton({ href, title }: MenuButtonProps) {
+  return (
+    <Button
+      asChild
+      className="bg-[#FCFCFC] hover:bg-[#DA8359] active:bg-[#DA8359] hover:text-white active:text-white text-black w-full h-16"
+    >
+      <Link href={href}>{title}</Link>
+    </Button>
+  )
+}
+
 export default function Home() {
   return (
-    <div className="bg-gradient-to-b from-[#F7BE7A] to-[#DA8359] h-screen">
+    <div className="flex flex-col bg-gradient-to-b from-[#F7BE7A] to-[#DA8359] min-h-screen">
       <NavBar />
       <Image
         src="/logo.png"
         alt="logo"
         width={200}
         height={200}
-        className="mx-auto"
+        className="mx-auto pb-24"
       />
 
-      <div className="flex flex-col gap-3 px-5 py-5 w-screen h-[50vh] rounded-t-2xl bg-[#F2F2F2] text-black">
-        <Link href="feeding/">
-          <Button className="bg-[#F7BE7A] hover:bg-[#DA8359] w-full">
-            Feed
-          </Button>
-        </Link>
-        <Link href="settings/">
-          <Button className="bg-[#F7BE7A] hover:bg-[#DA8359] w-full">
-            Settings
-          </Button>
-        </Link>
-        <Link href="sound/">
-          <Button className="bg-[#F7BE7A] hover:bg-[#DA8359] w-full">
-            Sound
-          </Button>
-        </Link>
+      <div className="flex flex-col flex-grow justify-between px-5 py-8 w-screen rounded-t-2xl bg-[#F2F2F2] text-black">
+        <div className="justify-center grid grid-cols-2 gap-3 gap-y-12">
+          <MenuButton href="feeding/" title="Feed" />
+          <MenuButton href="water/" title="Water" />
+          <MenuButton href="sound/" title="Sound" />
+          <MenuButton href="call/" title="Call" />
+        </div>
+        <Button
+          asChild
+          className="bg-[#DA8359] hover:bg-[#F7BE7A] active:bg-[#F7BE7A] w-full h-12"
+        >
+          <Link href="settings/">Settings</Link>
+        </Button>
       </div>
     </div>
   )

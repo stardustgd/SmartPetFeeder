6ba534d0c22f96e07ecf7fe4e91373f929280e17
stardust <88991140+stardustgd@users.noreply.github.com>
2024-12-17 18:10:11 -0800

    add stuff to the sound page

--- app/sound/page.tsx
+++ app/sound/page.tsx
@@ -1,13 +1,17 @@
 import NavBar from '@/components/ui/NavBar'
+import CustomCard from '@/components/ui/CustomCard'
 import { Button } from '@/components/ui/button'
+import { CardContent } from '@/components/ui/card'
 import {
   Drawer,
   DrawerContent,
   DrawerDescription,
+  DrawerFooter,
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
 } from '@/components/ui/drawer'
+import { FaPlus } from 'react-icons/fa'
 
 export default function SoundPage() {
   return (
@@ -29,6 +33,34 @@ export default function SoundPage() {
               <DrawerDescription>Modify your song settings.</DrawerDescription>
             </DrawerHeader>
           </div>
+          <div className="flex flex-col p-4 gap-5">
+            <CustomCard cardTitle="Comfort Song" />
+            <CustomCard cardTitle="Song 1 Schedule">
+              <CardContent>
+                <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg[#F7BE7A]" />
+                  <div className="space-y-2">
+                    <p className="leading-none">9:00 AM, Daily</p>
+                  </div>
+                </div>
+              </CardContent>
+            </CustomCard>
+            <CustomCard cardTitle="Song 2 Schedule">
+              <CardContent>
+                <div className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0">
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg[#F7BE7A]" />
+                  <div className="space-y-2">
+                    <p className="leading-none">6:00 PM, Friday</p>
+                  </div>
+                </div>
+              </CardContent>
+            </CustomCard>
+          </div>
+          <DrawerFooter className="mx-auto w-full max-w-sm">
+            <Button className="bg-[#8675B1] text-xl h-12">
+              <FaPlus />
+            </Button>
+          </DrawerFooter>
         </DrawerContent>
       </Drawer>
     </div>

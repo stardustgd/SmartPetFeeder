f2780f148fd66e301b53f6916dbfd31b402e32ad
stardust <88991140+stardustgd@users.noreply.github.com>
2025-02-02 15:46:27 -0800

    feat(FeedingPage): add ability to specify manual feeding amount

--- app/feeding/page.tsx
+++ app/feeding/page.tsx
@@ -1,5 +1,5 @@
-import CustomCard from '@/components/CustomCard'
 import NavBar from '@/components/NavBar'
+import ManualFeeding from '@/components/feeding/ManualFeeding'
 import ScheduledFeeding from '@/components/feeding/ScheduledFeeding'
 
 export default function FeedingPage() {
@@ -7,7 +7,7 @@ export default function FeedingPage() {
     <div className="flex flex-col bg-gradient-to-b from-[#F7BE7A] to-[#DA8359] min-h-screen">
       <NavBar title="Feeding" />
       <div className="flex flex-col flex-grow gap-5 px-5 py-5 w-screen rounded-t-2xl bg-[#F2F2F2] text-black">
-        <CustomCard cardTitle="Manual Feeding Amount" cardDescription="1 Cup" />
+        <ManualFeeding />
         <ScheduledFeeding />
       </div>
     </div>

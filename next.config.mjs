a31bd0dd8081672b534b4cd9be653642fd397998
stardust <88991140+stardustgd@users.noreply.github.com>
2025-02-18 21:31:13 -0800

    fix: fix api routes in development

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return process.env.NODE_ENV === 'development'
+      ? [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:5050/api/:path*',
+        },
+      ]
+      : []
+  },
+}
 
-export default nextConfig;
+export default nextConfig

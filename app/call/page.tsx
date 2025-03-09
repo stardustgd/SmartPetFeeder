21bb24ce6bb78f5759b14de04364429924ec2381
stardust <88991140+stardustgd@users.noreply.github.com>
2025-03-09 16:00:26 -0700

    feat: add camera feed to call page

--- app/call/page.tsx
+++ app/call/page.tsx
@@ -6,8 +6,13 @@ export default function CallPage() {
   return (
     <div className="flex flex-col bg-gradient-to-b from-[#F7BE7A] to-[#DA8359] min-h-screen">
       <NavBar title="Call" />
-      <div className="flex flex-col flex-grow gap-5 px-5 py-5 w-screen rounded-t-2xl bg-[#F2F2F2] text-black">
-        <div className="h-96 bg-gray-500"></div>
+      <div className="flex flex-col items-center flex-grow gap-5 px-5 py-5 w-screen rounded-t-2xl bg-[#F2F2F2] text-black">
+        <img
+          width={640}
+          height={480}
+          src={process.env.CAMERA_FEED_URL}
+          className="shadow-md"
+        />
         <div className="flex flex-row justify-center gap-12">
           <Button className="bg-[#A5B68D] rounded-full size-16">
             <FaCamera className="size-12" />

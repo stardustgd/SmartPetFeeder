cd2e0363f4ecfdd8f97fe59b3a155267a2b64e24
stardust <88991140+stardustgd@users.noreply.github.com>
2025-03-13 09:36:36 -0700

    fix: whitelist video feed URL in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,15 @@ const nextConfig = {
   async rewrites() {
     return process.env.NODE_ENV === 'development'
       ? [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:5050/api/:path*',
-        },
-      ]
+          {
+            source: '/api/:path*',
+            destination: 'http://localhost:5050/api/:path*',
+          },
+        ]
       : []
+  },
+  images: {
+    domains: ['spf.sebastian.sh'],
   },
 }
 

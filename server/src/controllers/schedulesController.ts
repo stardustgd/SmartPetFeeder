bcc3bcb4b7247916d8254eca768717d5defd796d
stardust <88991140+stardustgd@users.noreply.github.com>
2025-07-27 16:22:11 -0700

    refactor: implement schedules

--- server/src/controllers/schedulesController.ts
+++ server/src/controllers/schedulesController.ts
@@ -1,18 +1,23 @@
-import { Request, Response, NextFunction } from 'express'
+import { Response, NextFunction } from 'express'
 import connectMongo from '../middlewares/db'
+import { ObjectId } from 'mongodb'
+import { AuthenticatedRequest } from '../models/auth'
 
 export const getSchedules = async (
-  req: Request,
+  req: AuthenticatedRequest,
   res: Response,
   next: NextFunction
 ) => {
-  const { value } = req.params
+  if (!req.user) {
+    return res.status(401).json({ message: 'Invalid credentials' })
+  }
 
   try {
+    const user = ObjectId.createFromHexString(req.user.id.toString())
     const db = await connectMongo()
     const collection = db.collection('schedules')
 
-    const schedules = await collection.find({ userId: value }).toArray()
+    const schedules = await collection.find({ userId: user }).toArray()
 
     if (!schedules.length) {
       return res
@@ -26,6 +31,70 @@ export const getSchedules = async (
   }
 }
 
-// createSchedule
-// updateSchedule
-// deleteSchedule
+export const createSchedule = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.user) {
+    return res.status(401).json({ message: 'Invalid credentials' })
+  }
+
+  try {
+    const db = await connectMongo()
+    const collection = db.collection('schedules')
+
+    const user = ObjectId.createFromHexString(req.user.id.toString())
+    const { days, time, amount } = req.body
+
+    const existing = await collection.findOne({
+      userId: user,
+      days: days,
+      time: time,
+    })
+
+    if (existing) {
+      return res.status(409).json({ message: 'Schedule already exists' })
+    }
+
+    const newSchedule = {
+      userId: user,
+      days: days,
+      time: time,
+      amount: amount,
+    }
+
+    await collection.insertOne(newSchedule)
+
+    res.status(201).send({ message: 'Schedule successfully added' })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const deleteSchedule = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.user) {
+    return res.status(401).json({ message: 'Invalid credentials' })
+  }
+
+  if (!req.body.scheduleId) {
+    return res.status(400).json({ message: 'No scheduleId provided' })
+  }
+
+  try {
+    const db = await connectMongo()
+    const collection = db.collection('schedules')
+
+    const scheduleId = ObjectId.createFromHexString(req.body.scheduleId)
+
+    await collection.deleteOne({ _id: scheduleId })
+
+    res.status(200).send({ message: 'Schedule successfully deleted' })
+  } catch (error) {
+    next(error)
+  }
+}
--- server/src/routes/scheduleRoutes.ts
+++ server/src/routes/scheduleRoutes.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express'
-import { getSchedules } from '../controllers/schedulesController'
+import {
+  getSchedules,
+  createSchedule,
+  deleteSchedule,
+} from '../controllers/schedulesController'
+import { verifyToken } from '../middlewares/jwt'
 
 const router = Router()
 
-router.get('/:value', getSchedules)
+router.get('/', verifyToken, getSchedules)
+router.post('/', verifyToken, createSchedule)
+router.delete('/', verifyToken, deleteSchedule)
 
 export default router
